bbe6b4d39097d7d0a0b031ffe134e44e19720b48
Edgar <edgar@Edgars-MacBook-Air.local>
2024-07-24 15:50:44 -0500

    Add files

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Invoices = () => {
+  return <div>from invoices</div>;
+};
+
+export default Invoices;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div className="flex items-center justify-center">from dashboard</div>;
+};
+
+export default Dashboard;
